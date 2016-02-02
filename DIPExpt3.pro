adf2579361b11d1b2661c920c9c83f8eb9fbf8d1
Arnab Bag <amiarnabbolchi@gmail.com>
2016-02-02 16:52:18 +0530

    Added file listing part of the code

--- DIPExpt3.pro
+++ DIPExpt3.pro
@@ -5,6 +5,13 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
+INCLUDEPATH +="C:\Qt\OpenCV\install\include"
+INCLUDEPATH +="C:\Qt\OpenCV\install\include\opencv2\core"
+INCLUDEPATH +="C:\Qt\OpenCV\install\include\opencv2\highgui"
+INCLUDEPATH +="C:\Qt\OpenCV\install\include\opencv"
+
+LIBS+= C:/Qt/OpenCV/bin/*.dll
+
 include(deployment.pri)
 qtcAddDeployment()
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <core.hpp>
+#include <highgui.hpp>
+#include <cv.hpp>
+#include <dirent.h>
+#include <vector>
 
 using namespace std;
 
-int main()
+int main(int argc,char *argv[])
 {
-    cout << "Hello World!" << endl;
+    cout << "Starting program...\n";
+    DIR *mydir;
+    dirent *ent;
+    vector<string> inputfiles;
+
+    if(argc !=2)
+    {
+        cout << "Invalid no of arguments!\n";
+        return 1;
+    }
+
+    if(mydir = opendir(argv[1]))
+    {
+        while(ent = readdir(mydir))
+        {
+            string s = ent->d_name;
+            inputfiles.push_back(s);
+        }
+        closedir(mydir);
+        inputfiles.erase(inputfiles.begin(),inputfiles.begin()+2);
+        for(int i = 0;i<inputfiles.size();++i)
+        {
+            cout << inputfiles.at(i) << endl;
+        }
+    }
+    else
+    {
+        cout << "Cant open directory!\n";
+    }
     return 0;
 }
-
